289e374660f809b5437926ad575e51cfb7e815a8
andrerj2000 <andrerj2000@gmail.com>
2021-08-28 23:27:03 -0300

    Project implementation micro services

---
+++ digitalmicroservices/src/main/java/one/digitalInovation/digitalmicroservices/DigitalmicroservicesApplication.java
@@ -0,0 +1,13 @@
+package one.digitalInovation.digitalmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalmicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalmicroservicesApplication.class, args);
+	}
+
+}
---
+++ digitalmicroservices/src/test/java/one/digitalInovation/digitalmicroservices/DigitalmicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalInovation.digitalmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalmicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
